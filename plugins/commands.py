baf44614356e16a0bbc9bdd5fa6960928148f304
Moviesbotz <121341606+Moviesbotz@users.noreply.github.com>
2022-12-31 14:55:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -86,7 +86,7 @@ async def start(client, message):
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://shorturllink.in/st?api=45c5a4a773e132b44a5b2e9056d07befd949276e&url=https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
